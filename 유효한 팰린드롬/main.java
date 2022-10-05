11f18f46b7bd915600ca267bd864767cc9b7e56f
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-05 17:39:56 +0900

    Create main.java

---
+++ 유효한 팰린드롬/main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main {
+    public static String solution(String str) {
+        String answer = "NO";
+ 
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+ 
+        String tmp = new StringBuilder(str).reverse().toString();
+ 
+        if (str.equals(tmp)) {
+            answer = "YES";
+        }
+        return answer;
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+        String str = br.readLine();
+ 
+        System.out.println(solution(str));
+    }
+}
