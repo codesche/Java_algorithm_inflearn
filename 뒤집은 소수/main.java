c30ab6572da88b734bcb3d86b7e47b50cf7d4eba
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-16 23:11:06 +0900

    Create main.java

---
+++ 뒤집은 소수/main.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int tmp = arr[i];
+            int res = 0;
+            while (tmp > 0) {
+                int t = tmp % 10;
+                res = (res * 10) + t;
+                tmp = tmp / 10;
+            }
+            if (isPrime(res)) {
+                answer.add(res);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int x : T.solution(N, arr)) {
+            System.out.print(x + " ");
+        }
+
+    }
+}
