697699a86d832d8543a60a2feaabd1891d4e361e
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-18 16:23:39 +0900

    Create main.java

---
+++ 등수구하기/main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int cnt = 1;
+
+        // 배열 생성
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i] < arr[j]) {
+                    cnt++;
+                }
+            }
+            System.out.print(cnt + " ");
+            cnt = 1;
+        }
+    }
+}
