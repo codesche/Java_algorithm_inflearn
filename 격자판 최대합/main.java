0ec337cfe4e0e87d05987f1b8ec75e200e2f2159
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-19 11:27:14 +0900

    Create main.java

---
+++ 격자판 최대합/main.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public int solution(int n, int[][] arr) {
+        int answer = Integer.MIN_VALUE;
+        int sum1;
+        int sum2;
+        // sum1은 행
+        // sum2는 열
+
+        for (int i = 0; i < n; i++) {
+            sum1 = 0;
+            sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+        sum1 = 0;
+        sum2 = 0;
+
+        // 2중 for문을 이용할 필요 없음
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+        return answer;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.print(T.solution(N, arr));
+    }
+}
