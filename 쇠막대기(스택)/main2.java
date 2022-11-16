bfe56c67ed4568f7aca12bda6f4bbb498861d88c
Minsung <110509654+codesche@users.noreply.github.com>
2022-11-16 13:56:19 +0900

    Create main2.java

---
+++ 쇠막대기(스택)/main2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public int solution(String str) {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                stack.push('(');
+            } else {
+                stack.pop();
+                if (str.charAt(i - 1) == '(') {
+                    answer += stack.size();
+                } else {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(T.solution(str));
+    }
+}
